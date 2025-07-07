c582cf1f2d499c8b370d88fefe2ca6b2ca017e1d
Javier Muriel <javiermursal@gmail.com>
2025-07-08 01:04:40 +0200

    LAB acabado

--- DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantDetailScreen.js
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantDetailScreen.js
@@ -1,20 +1,91 @@
 /* eslint-disable react/prop-types */
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, View, FlatList, Pressable } from 'react-native'
+import { StyleSheet, View, FlatList, Image, ImageBackground } from 'react-native'
 import TextRegular from '../../components/TextRegular'
 import { API_BASE_URL } from '@env'
+import { getDetail } from '../../api/RestaurantEndpoints'
+import TextSemiBold from '../../components/TextSemibold'
+import * as GlobalStyles from '../../styles/GlobalStyles'
+import ImageCard from '../../components/ImageCard'
+import product from '../../../assets/product.jpeg'
 
-export default function RestaurantDetailScreen({ route }) {
-  const { id } = route.params
+export default function RestaurantDetailScreen ({ route }) {
+  const [restaurant, setRestaurant] = useState({})
+
+  useEffect(() => {
+    console.log('Loading restaurant details, please wait 1 second')
+    setTimeout(() => {
+      setRestaurant(getDetail(route.params.id))
+      console.log('Restaurant details loaded')
+    }, 1000)
+  }, [])
+
+  const renderProductWithImageCard = ({ item }) => {
+    return (
+      <ImageCard
+        imageUri={item.image ? { uri: API_BASE_URL + '/' + item.image } : product}
+        title={item.name}
+      >
+        <TextRegular numberOfLines={2}>{item.description}</TextRegular>
+        <TextSemiBold>Price: <TextSemiBold textStyle={{ color: GlobalStyles.brandPrimary }}>{item.price.toFixed(2)}€</TextSemiBold></TextSemiBold>
+      </ImageCard>
+    )
+  }
+
+  const renderHeader = () => {
+    return (
+      <ImageBackground source={(restaurant?.heroImage) ? { uri: process.env.API_BASE_URL + '/' + restaurant.heroImage, cache: 'force-cache' } : undefined } style={styles.imageBackground}>
+        <View style={styles.restaurantHeaderContainer}>
+            <TextSemiBold textStyle={styles.textTitle}>{restaurant.name}</TextSemiBold>
+            <Image style={styles.image} source={restaurant.logo ? { uri: process.env.API_BASE_URL + '/' + restaurant.logo, cache: 'force-cache' } : undefined} />
+            <TextRegular textStyle={styles.text}>{restaurant.description}</TextRegular>
+        </View>
+      </ImageBackground>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <TextRegular style={{ fontSize: 16, alignSelf: 'center', margin: 20 }}>Restaurant details. Id: {id}</TextRegular>
-    </View>
+    <FlatList
+      style={styles.container}
+      data={restaurant.products}
+      ListHeaderComponent={renderHeader}
+      renderItem={renderProductWithImageCard}
+      keyExtractor={item => item.id.toString()}
+    />
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  row: {
+    padding: 15,
+    marginBottom: 5,
+    backgroundColor: GlobalStyles.brandSecondary
+  },
+  restaurantHeaderContainer: {
+    height: 250,
+    padding: 20,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  imageBackground: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center'
+  },
+  image: {
+    height: 100,
+    width: 100,
+    margin: 10
+  },
+  text: {
+    color: 'white'
+  },
+  textTitle: {
+    fontSize: 20,
+    color: 'white'
   }
 })
--- DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantsScreen.js
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantsScreen.js
@@ -1,34 +1,49 @@
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, View, FlatList, Pressable } from 'react-native'
+import { StyleSheet, FlatList } from 'react-native'
 import TextRegular from '../../components/TextRegular'
 import { getAll } from '../../api/RestaurantEndpoints'
 import * as GlobalStyles from '../../styles/GlobalStyles'
 import TextSemiBold from '../../components/TextSemibold'
 import { API_BASE_URL } from '@env'
 import restaurantLogo from '../../../assets/restaurantLogo.jpeg'
+import ImageCard from '../../components/ImageCard'
 
-export default function RestaurantsScreen({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <TextRegular style={{ fontSize: 16, alignSelf: 'center', margin: 20 }}>Random Restaurant</TextRegular>
-      <Pressable
+export default function RestaurantsScreen ({ navigation }) {
+  const [restaurants, setRestaurants] = useState([])
+
+  useEffect(() => {
+    console.log('Loading restaurants, please wait 2 seconds')
+    setTimeout(() => {
+      setRestaurants(getAll) // getAll function has to be imported
+      console.log('Restaurants loaded')
+    }, 2000)
+  }, [])
+
+  const renderRestaurantWithImageCard = ({ item }) => {
+    return (
+      <ImageCard
+        imageUri={item.logo ? { uri: API_BASE_URL + '/' + item.logo } : restaurantLogo}
+        title={item.name}
         onPress={() => {
-          navigation.navigate('RestaurantDetailScreen', { id: Math.floor(Math.random() * 100) })
+          navigation.navigate('RestaurantDetailScreen', { id: item.id })
         }}
-        style={({ pressed }) => [
-          {
-            backgroundColor: pressed
-              ? GlobalStyles.brandBlueTap
-              : GlobalStyles.brandBlue
-          },
-          styles.actionButton
-        ]}
       >
-        <TextRegular textStyle={styles.text}>
-          Go to Random Restaurant Details
-        </TextRegular>
-      </Pressable>
-    </View>
+        <TextRegular numberOfLines={2}>{item.description}</TextRegular>
+        {item.averageServiceMinutes !== null &&
+          <TextSemiBold>Avg. service time: <TextSemiBold textStyle={{ color: GlobalStyles.brandPrimary }}>{item.averageServiceMinutes} min.</TextSemiBold></TextSemiBold>
+        }
+        <TextSemiBold>Shipping: <TextSemiBold textStyle={{ color: GlobalStyles.brandPrimary }}>{item.shippingCosts.toFixed(2)}€</TextSemiBold></TextSemiBold>
+      </ImageCard>
+    )
+  }
+
+  return (
+      <FlatList
+        style={styles.container}
+        data={restaurants}
+        renderItem={renderRestaurantWithImageCard}
+        keyExtractor={item => item.id.toString()}
+      />
   )
 }
 
